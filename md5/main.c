8a97b5db3f3e269d6791a01f62a9d0c7b662e14c
bianyuan1986 <231881831@qq.com>
2020-04-16 18:00:23 +0800

    update

--- md5/main.c
+++ md5/main.c
@@ -18,6 +18,9 @@ static unsigned char shift[64];
 #define MD5_HMAC_BLOCK_SIZE	64
 #define MD5_BLOCK_WORDS		16
 #define MD5_HASH_WORDS		4
+//#define SECTION_VAR __attribute__((section(".data"))) 
+#define SECTION_VAR 
+
 
 #define MD5STEP(f, w, x, y, z, in, s) \
 	(w += f(x, y, z) + in, w = (w<<s | w>>(32-s)) + x)
@@ -68,24 +71,24 @@ static void md5_transform(u32 *hash, u32 const *in)
 	d = hash[3];
 
 
-	static unsigned char g[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+	static unsigned char g[] SECTION_VAR = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
 								1, 6, 11, 0, 5, 10, 15, 4, 9, 14, 3, 8, 13, 2, 7, 12,
 								5, 8, 11, 14, 1, 4, 7, 10, 13, 0, 3, 6, 9, 12, 15, 2,
 								0, 7, 14, 5, 12, 3, 10, 1, 8, 15, 6, 13, 4, 11, 2, 9};
 
-	static unsigned int k[] = {0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+	static unsigned int k[] SECTION_VAR = {0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
 								0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8, 0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
 								0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70, 0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
 								0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
 	};
 
-	static unsigned char shift[] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+	static unsigned char shift[] SECTION_VAR = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
 									5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
 									4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
 									6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
 									};
 
-	static f fTable[] = {F1, F2, F3, F4};
+	static f fTable[] SECTION_VAR = {F1, F2, F3, F4};
 
 	for( ; i < 64; i++)
 	{
@@ -171,9 +174,12 @@ char *getExeFile()
 	int fd = -1;
 	struct stat st;
 	static char path[PATH_LEN];
+	static unsigned long long int format1[2] SECTION_VAR;
 
 	pid = getpid();
-	snprintf(path, PATH_LEN, "/proc/%d/exe", pid);
+	format1[0] = 0x64252f636f72702f;
+	format1[1] = 0x000000006578652f;
+	snprintf(path, PATH_LEN, (char*)format1, pid);
 	stat(path, &st);
 	fd = open(path, O_RDONLY);
 	read(fd, fileContent, st.st_size);
@@ -185,7 +191,10 @@ int main(int argc, char *argv[])
 	struct md5_ctx ctx;
 	int i = 0;
 	unsigned char *md5 = NULL;
+	static unsigned long long int format[3] SECTION_VAR;
 
+	format[0] = format[1] = 0x7832302578323025;
+	format[2] = 0;
 	getExeFile();
 	md5_init(&ctx);
 	md5_update(&ctx, fileContent, fileContentLen);
@@ -193,10 +202,11 @@ int main(int argc, char *argv[])
 	md5 = (unsigned char*)ctx.hash;
 	for( ; i < 16; i += 4)
 	{
-		printf("%02x%02x%02x%02x", md5[i], md5[i+1], md5[i+2], md5[i+3]);
+		printf((char*)format, md5[i], md5[i+1], md5[i+2], md5[i+3]);
 	}
 	printf("\n");
 
-	return 0;
+	//return 0;
+	_exit(0);
 }
 
